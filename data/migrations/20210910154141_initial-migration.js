9a82a3f70eba908a67f68f23970f1d6916d9d760
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-10 11:07:34 -0500

    Add resources and tasks table scaffolding

--- data/migrations/20210910154141_initial-migration.js
+++ data/migrations/20210910154141_initial-migration.js
@@ -8,9 +8,21 @@ exports.up = async function(knex) {
     })
     .createTable('resources', table => {
       table.increments('resource_id')
+      table.string('resource_name', 80).notNullable().unique()
+      table.string('resource_description', 200)
     })
     .createTable('tasks', table => {
       table.increments('task_id')
+      table.string('task_description').notNullable()
+      table.string('task_notes')
+      table.boolean('task_completed').defaultTo(0)
+      table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
     })
     .createTable('project_resources', table => {
       table.increments()
