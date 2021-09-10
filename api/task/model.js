3081010a8aa2880182ac4a4b43232f301f5a83d2
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-10 15:59:45 -0500

    WIP

--- api/task/model.js
+++ api/task/model.js
@@ -1,13 +1,27 @@
 const db = require('../../data/dbConfig')
 
-function getTasks() {
-  return db('tasks')
+async function getTasks() {
+  const rows = await db('tasks as t')
+    .leftJoin('projects as p', 't.project_id', 'p.project_id')
+    .select('task_id', 'task_description', 'task_notes', 'task_completed', 'project_name', 'project_description')
+  const result = rows.map(row => ({
+    task_id: row.task_id,
+    task_notes: row.task_notes,
+    task_description: row.task_description,
+    task_completed: row.task_completed ? true : false,
+    project_name: row.project_name,
+    project_description: row.project_description,
+  }))
+  return result
 }
 
 function addTask(task) {
   return db('tasks').insert(task)
     .then(([task_id]) => {
       return db('tasks').where('task_id', task_id).first()
+    })
+    .then(newTask => {
+      return {...newTask, task_completed: newTask.task_completed ? true : false}
     })
 }
 
